2bcc5e51c504567147811398be5be3377142cd44
theRealCooller <jakublohnicky@hotmail.com>
2022-07-11 15:04:45 +0200

    add string model representation

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,9 @@ class Project(models.Model):
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        """
+        model string representation
+        """
+        return self.title
