8708cac8cad6fe7328f75f8e26f3a3a1935a5604
theRealCooller <jakublohnicky@hotmail.com>
2022-08-09 15:01:30 +0200

    add login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from .models import Profile
 
 
-def loginPage(request):
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('User not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('projects')
+
     return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def profiles(request):
