92a77bf3d2d43f7f4eed7d7b40ef626a6904f1b8
theRealCooller <jakublohnicky@hotmail.com>
2022-08-24 17:12:46 +0200

    render message contents to the template

--- users/views.py
+++ users/views.py
@@ -136,5 +136,7 @@ def inbox(request):
 
 @login_required(login_url='login')
 def message(request, pk):
-    context = {}
+    profile = request.user.profile
+    message =profile.messages.get(id=pk)
+    context = {'message': message}
     return render(request, 'users/message.html', context)
