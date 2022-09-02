b13aa095c6004692f92d11cae7f4d56bdef66c09
theRealCooller <jakublohnicky@hotmail.com>
2022-09-02 18:31:13 +0200

    serialize api data, nesting objects into objects

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project, Tag
+from users.models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    serialize users model data
+    """
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    serialize tag model data
+    """
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    """
+    serialize Project model data and set some model fields to another serialized objects to gain
+    access to dynamic data thru project serializer itself
+    """
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+
     class Meta:
         model = Project
         fields = '__all__'
